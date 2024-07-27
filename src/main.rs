31a6e4dbb8885180dc1a88055e14f827c416a4f4
RePloZ <herve.kay@mailo.com>
2024-07-27 18:58:43 +0200

    feat: error route page

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::get,
+    Router,
+    http::{StatusCode}
+};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn internal_error() -> StatusCode {
+    StatusCode::INTERNAL_SERVER_ERROR
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
-
+    let router = Router::new()
+        .route("/-1/error", get(internal_error))
+        .route("/", get(hello_world));
     Ok(router.into())
 }
